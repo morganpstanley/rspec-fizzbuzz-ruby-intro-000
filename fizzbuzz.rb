2ebead4b08bf4b032cd5a34659e306e846041b91
Morgan Stanley <morganpstanley@gmail.com>
2019-06-15 01:23:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if (num % 3 == 0 && num % 5 == 0)
-    return FizzBuzz
+    return "FizzBuzz"
   elsif num % 3 == 0
-    return Fizz
+    return "Fizz"
   elsif num % 5 == 0
-    return Buzz
+    return "Buzz"
   else
     return nil
   end
